da7b8866af5f259a9b2ad65c63716d8c87ed3412
Olga <osa_007@mail.ru>
2022-09-01 22:34:16 +0300

    added task1 for dz7

---
+++ Task001/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+Console.Clear();
+int rous = NumberFromUser ("Введите количество строк массива: ","Ошибка ввода!");
+int columns = NumberFromUser ("Введите количество столбцов массива: ","Ошибка ввода!");
+double[,] array = GetArray(rous, columns);
+PrintArray (array);
+
+// возвращает количество элементов (строк и столбцов) массива, либо сообщение об ошибке
+
+int NumberFromUser (string message, string errorMessage)
+{
+    while(true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+// возвращает массив размером mxn 
+
+double [,] array GetArray (int m, int n)
+{
+    double[] result = new double[m, n];
+    Random rand = new Random();
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i,j] = rand.NextDouble();
+        }
+    }
+    return result;
+}
+
+// выводит массив в консоль
+
+void PrintArray (double [,] inArray)
+{
+for (int i = 0; i < inArray.Getlength(0); i++)
+{
+    for (int j = 0; j < inArray.Getlength(1); j++)
+    {
+       Console.Write($"{inArray[i,j]}"); 
+    }
+    Console.WriteLine();
+}
+}
